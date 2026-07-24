d8492b997a81003ef3feae56ebbd6a19d02a5c02
Neznetwork <miguelnarede@gmail.com>
2026-07-23 21:10:37 -0300

    feat-script da pesquisa e strikethrough

--- script.js
+++ script.js
@@ -1,27 +1,45 @@
-let lista= document.getElementById("lista");
-let textbox= document.getElementById("textbox");
+let lista = document.getElementById("lista");
+let textbox = document.getElementById("textbox");
 
-function newElement() {
-var li = document.createElement("li");
-var inputValue = document.getElementById("textbox").value;
-var novoItem = document.createTextNode(inputValue);
+function filterByName(event) {
+  const searchTerm = event.target.value.toLowerCase();
+  const listItems = document.querySelectorAll("#lista li");
 
- li.appendChild(novoItem);
- if (inputValue === '') {
-    alert("You must write something!");
-  } else {
-var btnExcluir = document.createElement("button");
-  btnExcluir.innerText = "excluir";
-
-  btnExcluir.addEventListener("click", function(){
-    li.remove();
+  listItems.forEach(function(item) {
+    const text = item.innerText.toLowerCase();
+    item.style.display = text.includes(searchTerm) ? "" : "none";
   });
-
-  li.appendChild(btnExcluir);
-
-    document.getElementById("lista").appendChild(li);
-  }
-  document.getElementById("textbox").value = "";
-
 }
 
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("textbox").value;
+  var novoItem = document.createTextNode(inputValue);
+
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+
+  checkbox.addEventListener('change', function() {
+    li.style.textDecoration = checkbox.checked ? 'line-through' : 'none';
+  });
+
+  li.appendChild(checkbox);
+
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    li.appendChild(novoItem);
+
+    var btnExcluir = document.createElement("button");
+    btnExcluir.innerText = "excluir";
+
+    btnExcluir.addEventListener("click", function() {
+      li.remove();
+    });
+
+    li.appendChild(btnExcluir);
+    document.getElementById("lista").appendChild(li);
+  }
+
+  document.getElementById("textbox").value = "";
+}
